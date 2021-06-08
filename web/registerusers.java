4baf6b094cec3a3d1f50bc845c3063a4c91ed405
BhagyashriGolhar <85004328+BhagyashriGolhar@users.noreply.github.com>
2021-06-08 09:26:58 +0530

    Update registerusers.java

--- web/registerusers.java
+++ web/registerusers.java
@@ -55,107 +55,15 @@ public class registerusers extends HttpServlet {
                 stmt=conn.createStatement();
                 stmt.executeUpdate("insert into users (firstname,lastname,email,password1,password2,mobnum)"
                         + "values('"+fname+"','"+lname+"','"+email+"','"+pass1+"','"+pass2+"','"+mob+"')");
-                     out.write("<!DOCTYPE html>\n" +
-"<!--\n" +
-"To change this license header, choose License Headers in Project Properties.\n" +
-"To change this template file, choose Tools | Templates\n" +
-"and open the template in the editor.\n" +
-"-->\n" +
-"<html>\n" +
-"    <head>\n" +
-"        <title>welcome</title>\n" +
-"        \n" +
-"        <meta charset=\"UTF-8\">\n" +
-"        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-"        <link rel=\"stylesheet\" href=\"logincss.css\">\n" +
-"        <script type=\"text/javascript\" src =\"loginjs.js\"></script>\n" +
-"    </head>\n" +
-"    <body>\n" +
-"        <main id=\"main-holder\">\n" +
-"            \n" +
-"        <form name=\"form1\" id=\"login-form\">\n" +
-"            <div class=\"form-body\">\n" +
-"              \n" +
-"            <div class=\"form-group1\">\n" +
-"                <h1 id=\"login-header\">Login</h1>\n" +
-"            </div>\n" +
-"            <div class=\"form-group\">\n" +
-"            <label for=\"username\" class=\"label-title\">Username(Email)</label>\n" +
-"            <input type=\"text\" name=\"userid\" class=\"form-input\" id=\"username-field\" placeholder=\"Enter username\">\n" +
-"            </div>\n" +
-"            \n" +
-"            <div class=\"form-group\">\n" +
-"            <label for=\"password\" class=\"label-title\">Password</label>\n" +
-"            <input type=\"password\" name=\"passw\" class=\"form-input\" id=\"password-field\" placeholder=\"Enter password\">\n" +
-"            </div>\n" +
-"            \n" +
-"            <input type=\"submit\" value=\"Login\" id=\"login-form-submit\" formaction=\"olduser\" >\n" +
-"            </div>\n" +
-"            \n" +
-"            <p>Don't have an account,<a href=\"registration.html\">click here</a></p>\n" +
-"        </form>\n" +
-"        </main>\n" +
-"    </body>\n" +
-"</html>\n" +
-"");
+                     
               
                      
+                     response.sendRedirect("index.html");
                      
                      
                      
                      
                      
-                     /*  out.write("<html> ");
-                 out.write("<head> ");
-                 out.print("<link href='index.html'/>");
-                 out.print("<style> html\n" +
-"{\n" +
-"  height: 100%;  \n" +
-"}\n" +
-"\n" +
-"body\n" +
-"{\n" +
-"     background: linear-gradient(to right, #78a7ba 0%, #385d6c 50%, #78a7ba 99%);\n" +
-"    height :100%;\n" +
-"    margin: 0;\n" +
-"    font-family: Arial,Helvetica,sans-serif;\n" +
-"    display: grid;\n" +
-"    justify-items: center;\n" +
-"    align-items: center;\n" +
-
-"}\n" +
-"   #login-button { width: 100%;\n" +
-"    padding: 10px;\n" +
-"    border: none;\n" +
-"    border-radius: 7px;\n" +
-"    color: white;\n" +
-"    font-weight: bold;\n" +
-     "font-size: 30px;\n" +
-"    background-color:  #1bba93;\n" +
-"    cursor: pointer;\n" +
-"    outline: none; } #login-form\n" +
-"{\n" +
-"    align-self: flex-start;\n" +
-"    display: grid;\n" +
-"    justify-items: center;\n" +
-"    align-items: center;\n" +
-"} .form-body {\n" +
-"    padding: 10px 40px;\n" +
-"    color: #666;\n" +
-"}  #main-holder\n" +
-"{\n" +
-"    width: 40%;\n" +
-"    height: 60%;\n" +
-"    display: grid;\n" +
-"    justify-items: center;\n" +
-"    background-color: white;\n" +
-"    border-radius: 7px;\n" +
-"    box-shadow: 0px 0px 5px 5px black;\n" +
-"}</style>  <body>  <main id=\"main-holder\"> <form action='index.html' id=login-form> <div class=\"form-body\">  <img src='welcome.jpg' width='340' height='200'> </br></br></br></br><input type='submit' value='Click to Login' id='login-button'> </div> </form> </main></body>");
-                   out.write (" </head>");
-                   out.write(" </html>");*/
-             
-             
             
              
             }
